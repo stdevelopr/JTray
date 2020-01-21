a0c8f12c6a0d896968fa956b5c423cf34c4f0491
richard <richard.turra@sensedata.com.br>
2020-01-21 15:07:32 -0300

    dnd list and card, no state update

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -4,20 +4,14 @@ import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import styled from "styled-components";
 
-const CardContainer = styled.div`
-  margin-bottom: 8px;
-`;
-
 export const JtrayCard = ({ text }) => (
   <div>
-    <CardContainer>
-      <Card>
-        <CardContent>
-          <Typography color="textSecondary" gutterBottom>
-            {text}
-          </Typography>
-        </CardContent>
-      </Card>
-    </CardContainer>
+    <Card>
+      <CardContent>
+        <Typography color="textSecondary" gutterBottom>
+          {text}
+        </Typography>
+      </CardContent>
+    </Card>
   </div>
 );
--- frontend/src/components/List.jsx
+++ frontend/src/components/List.jsx
@@ -3,6 +3,7 @@ import { JtrayCard } from "./Card.jsx";
 import styled from "styled-components";
 import { ListContext } from "../ListContext";
 import { AddButton } from "./ActionButton.jsx";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 const ListContainer = styled.div`
   background-color: #dfe3e6;
@@ -18,20 +19,68 @@ const Board = styled.div`
   height: 100%;
 `;
 
+const CardContainer = styled.div`
+  margin-bottom: 8px;
+`;
+
 export const ListBoard = () => {
   const [lists, setList] = useContext(ListContext);
   return (
-    <Board>
-      {lists.map(list => (
-        <ListContainer key={list.id}>
-          <h3>{list.title}</h3>
-          {list.cards.map(card => (
-            <JtrayCard text={card.text} key={card.id} />
-          ))}
-          <AddButton />
-        </ListContainer>
-      ))}
-      <AddButton list />
-    </Board>
+    <DragDropContext>
+      <Droppable droppableId="all-lists" type="list" direction="horizontal">
+        {provided => (
+          <Board {...provided.droppableProps} ref={provided.innerRef}>
+            {lists.map((list, index) => (
+              <Draggable
+                draggableId={String(list.id)}
+                index={index}
+                key={list.id}
+              >
+                {provided => (
+                  <ListContainer
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                  >
+                    <Droppable droppableId={String(list.id)}>
+                      {provided => (
+                        <div
+                          key={list.id}
+                          {...provided.droppableProps}
+                          ref={provided.innerRef}
+                        >
+                          <h3>{list.title}</h3>
+                          {list.cards.map((card, index) => (
+                            <Draggable
+                              draggableId={String(card.id)}
+                              index={index}
+                              key={card.id}
+                            >
+                              {provided => (
+                                <CardContainer
+                                  ref={provided.innerRef}
+                                  {...provided.draggableProps}
+                                  {...provided.dragHandleProps}
+                                >
+                                  <JtrayCard text={card.text} key={card.id} />
+                                </CardContainer>
+                              )}
+                            </Draggable>
+                          ))}
+                          {provided.placeholder}
+                          <AddButton />
+                        </div>
+                      )}
+                    </Droppable>
+                  </ListContainer>
+                )}
+              </Draggable>
+            ))}
+            {provided.placeholder}
+            <AddButton list />
+          </Board>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 };
